a8a76151aedc85436a4914d908b3da5d0b86e24c
Haymanot Demis <haymanotdemis1@gmail.com>
2023-08-15 08:27:50 +0300

    adding automapper to the blog post web api

--- WebApplication1/WebApplication1/Middlewares/AutomapperProfile.cs
+++ WebApplication1/WebApplication1/Middlewares/AutomapperProfile.cs
@@ -5,30 +5,27 @@ class AutomapperProfile : Profile
 {
     public AutomapperProfile()
     {
-        CreateMap<Comment, CreateCommentDto>();
-        CreateMap<CreateCommentDto, Comment>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => {
+        CreateMap<CreateCommentDto, Comment>().ReverseMap().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => {
             if (srcMember is int value && value == 0) {
                 return false;
             }
             return srcMember != null;
         }));
-        CreateMap<Post, CreatePostDto>();
-        CreateMap<CreatePostDto, Post>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => {
+        
+        CreateMap<CreatePostDto, Post>().ReverseMap().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => {
             if (srcMember is int value && value == 0) {
                 return false;
             }
             return srcMember != null;
         }));
 
-        CreateMap<Post, UpdatePostsDto>();
-        CreateMap<UpdatePostsDto, Post>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>{
+        CreateMap<UpdatePostsDto, Post>().ReverseMap().ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>{
             if (srcMember is int value && value == 0) {
                 return false;
             }
             return srcMember != null;
         }));
-        CreateMap<Comment, UpdateCommentDto>();
-        CreateMap<UpdateCommentDto, Comment>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => {
+        CreateMap<UpdateCommentDto, Comment>().ReverseMap().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => {
             if (srcMember is int value && value == 0) {
                 return false;
             }
